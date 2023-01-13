82b91bc6faac522bd064c0121f62e721752e90f3
YamaHaya/山隼 <114995011+Yama-Haya@users.noreply.github.com>
2023-01-13 10:52:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// MENUボタンがクリックされたときの処理
-$('#menu_btn').on('click', function(){
-  SlideMenu()
-});
 
-// スライドメニュー関数
-const SlideMenu = () => {
-  if($('#menu').hasClass("SlideOut")){
-    // スライドメニューが非表示なら表示
-    $('#menu').removeClass('SlideOut');
-    $('#menu').addClass('SlideIN');
-  } else {
-    // スライドメニューがあれば非表示
-    $('#menu').removeClass('SlideIN');
-    $('#menu').addClass('SlideOut');
-  }
-}
